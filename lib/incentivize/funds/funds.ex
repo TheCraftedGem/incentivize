68acd84771981843e4fce06b796446984a945bd7
Bryan Joseph <bryanjos@gmail.com>
2018-08-09 18:42:49 -0500

    Add comment

--- lib/incentivize/funds/funds.ex
+++ lib/incentivize/funds/funds.ex
@@ -26,6 +26,7 @@ defmodule Incentivize.Funds do
   def github_actions do
     [
       "pull_request.opened": "Pull Request Opened",
+      # NOTE: 'pull_request.closed' covers both closed and merged PRs. Be sure to check the payload of PR webhook to see if it was merged or just closed
       "pull_request.closed": "Pull Request Closed",
       "issue_comment.created": "Issue Commented",
       "issues.opened": "Issue Opened",
