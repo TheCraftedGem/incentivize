5d3632e37414f1e5ee1412b7583d9414a8e87c6a
Bryan Joseph <bryanjos@gmail.com>
2018-08-13 20:23:08 -0500

    Add contribution count

--- lib/incentivize/funds/contributions.ex
+++ lib/incentivize/funds/contributions.ex
@@ -4,11 +4,33 @@ defmodule Incentivize.Contributions do
   """
 
   import Ecto.{Query}, warn: false
-  alias Incentivize.{Contribution, Repo}
+  alias Incentivize.{Contribution, Fund, Pledge, Repo}
 
   def create_contribution(params) do
     %Contribution{}
     |> Contribution.changeset(params)
     |> Repo.insert()
+  end
+
+  def list_contributions_for_repository(repository) do
+    Contribution
+    |> where([c], c.repository_id == ^repository.id)
+    |> order_by([c], c.inserted_at)
+    |> Repo.all()
+  end
+
+  def list_contributions_for_fund(fund) do
+    query =
+      from(c in Contribution,
+        join: pledge in Pledge,
+        on: c.pledge_id == pledge.id,
+        join: fund in Fund,
+        on: pledge.fund_id == fund.id,
+        where: fund.id == ^fund.id,
+        order_by: c.inserted_at,
+        select: c
+      )
+
+    Repo.all(query)
   end
 end
