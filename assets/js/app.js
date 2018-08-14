fdb1f2a2417ae67a85a6a040ec678be0d818ef96
Bryan Joseph <bryanjos@gmail.com>
2018-08-14 18:06:30 -0500

    Placeholders, Validations, and Helpers

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,10 +64,37 @@ function setupMenu() {
   }
 }
 
+function setupConnectRepositoryHelper() {
+  const ownerSource = document.querySelector('#repository_owner')
+  const nameSource = document.querySelector('#repository_name')
+
+  const ownerSink = document.querySelector('[data-repository-owner]')
+  const nameSink = document.querySelector('[data-repository-name]')
+
+  if (ownerSource && ownerSink && nameSource && nameSink) {
+    ownerSource.addEventListener('input', () => {
+      if (ownerSource.value !== '') {
+        ownerSink.innerHTML = ownerSource.value
+      } else {
+        ownerSink.innerHTML = '[owner]'
+      }
+    })
+
+    nameSource.addEventListener('input', () => {
+      if (nameSource.value !== '') {
+        nameSink.innerHTML = nameSource.value
+      } else {
+        nameSink.innerHTML = '[name]'
+      }
+    })
+  }
+}
+
 function init() {
   setupMenu()
   setupFundForm()
   getStellarBalances()
+  setupConnectRepositoryHelper()
 }
 
 document.addEventListener('DOMContentLoaded', init)
