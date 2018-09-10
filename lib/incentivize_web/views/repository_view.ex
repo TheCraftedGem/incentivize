bc83bc2cfa8841b3448a019007b0b5bfcb94fc0f
Bryan Joseph <bryanjos@gmail.com>
2018-09-10 13:37:41 -0500

    Remove fake copy

--- lib/incentivize_web/views/repository_view.ex
+++ lib/incentivize_web/views/repository_view.ex
@@ -1,8 +1,3 @@
 defmodule IncentivizeWeb.RepositoryView do
   use IncentivizeWeb, :view
-  alias Incentivize.{Contributions}
-
-  def count_contributions_for_repository(repo) do
-    length(Contributions.list_contributions_for_repository(repo))
-  end
 end
