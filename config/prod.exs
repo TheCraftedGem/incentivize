37e96cb1ffbc50eaaf45d7b35e3122fb45a15f63
Bryan Joseph <bryanjos@gmail.com>
2018-09-11 09:52:26 -0500

    Update url settings for phoenix in production

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :incentivize, IncentivizeWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: System.get_env("APP_DOMAIN"), port: 443],
+  url: [host: "localhost", port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
