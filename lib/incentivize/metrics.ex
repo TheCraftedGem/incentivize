aa8824c90f9d807d3fe61ed5f31d06fdfc19f390
Bryan Joseph <bryanjos@gmail.com>
2018-09-13 16:29:59 -0500

    Use Incentivize.Statix explicitly

--- lib/incentivize/metrics.ex
+++ lib/incentivize/metrics.ex
@@ -1,13 +1,14 @@
 defmodule Incentivize.Metrics do
   @moduledoc false
-  alias Incentivize.Statix
   require Logger
 
   @behaviour :vmstats_sink
 
   def collect(_type, name, value) do
     try do
-      Statix.gauge(IO.iodata_to_binary(name), value, tags: Statix.base_tags())
+      Incentivize.Statix.gauge(IO.iodata_to_binary(name), value,
+        tags: Incentivize.Statix.base_tags()
+      )
     rescue
       ArgumentError ->
         Logger.warn("Incentivize.Metrics.collect failed")
@@ -20,7 +21,7 @@ defmodule Incentivize.Metrics do
   def record_ecto_metric(entry) do
     try do
       tags =
-        Statix.base_tags() ++
+        Incentivize.Statix.base_tags() ++
           [
             "query:#{entry.query}",
             "table:#{entry.source}"
@@ -29,14 +30,14 @@ defmodule Incentivize.Metrics do
       queue_time = entry.queue_time || 0
       duration = entry.query_time + queue_time
 
-      Statix.increment(
+      Incentivize.Statix.increment(
         "ecto.query.count",
         1,
         tags: tags
       )
 
-      Statix.histogram("ecto.query.exec.time", duration, tags: tags)
-      Statix.histogram("ecto.query.queue.time", queue_time, tags: tags)
+      Incentivize.Statix.histogram("ecto.query.exec.time", duration, tags: tags)
+      Incentivize.Statix.histogram("ecto.query.queue.time", queue_time, tags: tags)
     rescue
       ArgumentError ->
         Logger.warn("Incentivize.Metrics.record_ecto_metric failed")
--- lib/incentivize_web/instrumenter.ex
+++ lib/incentivize_web/instrumenter.ex
@@ -1,23 +1,26 @@
 defmodule IncentivizeWeb.Instrumenter do
   @moduledoc false
 
-  alias Incentivize.Statix
-
   @doc false
   def phoenix_controller_call(:start, _compile_metadata, runtime_metadata) do
-    Statix.increment("phoenix.request.count", 1, tags: tags(runtime_metadata.conn))
+    Incentivize.Statix.increment("phoenix.request.count", 1, tags: tags(runtime_metadata.conn))
 
     runtime_metadata
   end
 
   @doc false
   def phoenix_controller_call(:stop, diff, result_of_before_callback) do
-    Statix.increment("phoenix.response.count", 1, tags: tags(result_of_before_callback.conn))
-    Statix.histogram("phoenix.response.time", diff, tags: tags(result_of_before_callback.conn))
+    Incentivize.Statix.increment("phoenix.response.count", 1,
+      tags: tags(result_of_before_callback.conn)
+    )
+
+    Incentivize.Statix.histogram("phoenix.response.time", diff,
+      tags: tags(result_of_before_callback.conn)
+    )
   end
 
   defp tags(conn) do
-    Statix.base_tags() ++
+    Incentivize.Statix.base_tags() ++
       [
         "method:#{conn.method}",
         "path:#{conn.request_path}",
