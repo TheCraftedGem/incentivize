fb513b4d7d563e6894e04adb0db5980597994a2c
Bryan Joseph <bryanjos@gmail.com>
2018-09-21 14:53:53 -0500

    Remove references to UserFund and UserRepositor

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,6 @@
 defmodule Incentivize.Factory do
   @moduledoc false
-  alias Incentivize.{Actions, Fund, Github.Installation, Pledge, Repo, Repository, User, UserFund, UserRepository}
+  alias Incentivize.{Actions, Fund, Github.Installation, Pledge, Repo, Repository, User}
   @stellar_module Application.get_env(:incentivize, :stellar_module)
 
   def build(:user) do
@@ -33,25 +33,11 @@ defmodule Incentivize.Factory do
     }
   end
 
-  def build(:user_repository) do
-    %UserRepository{
-      user: build(:user),
-      repository: build(:repository)
-    }
-  end
-
   def build(:fund) do
     %Fund{
       repository: build(:repository),
       pledges: [build(:pledge)],
       created_by: build(:user)
-    }
-  end
-
-  def build(:user_fund) do
-    %UserFund{
-      user: build(:user),
-      fund: build(:fund)
     }
   end
 
